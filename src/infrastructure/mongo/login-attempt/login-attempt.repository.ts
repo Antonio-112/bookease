11eae41ace5803ba3e27381d4b78db32b4f40883
Antonio Aviles <71448936+Antonio-112@users.noreply.github.com>
2023-04-17 22:33:39 -0400

    Fixed login schema and added logs/comments

--- src/infrastructure/mongo/login-attempt/login-attempt.repository.ts
+++ src/infrastructure/mongo/login-attempt/login-attempt.repository.ts
@@ -3,25 +3,34 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { LoginAttempt } from '../../../domain/login-attempt/login-attempt.entity';
 import { ILoginAttemptRepository } from '../../../domain/login-attempt/login-attempt.repository';
+import { Logger } from '@nestjs/common';
 
 @Injectable()
 export class LoginAttemptRepository implements ILoginAttemptRepository {
+  private readonly logger = new Logger(LoginAttemptRepository.name);
+
   constructor(
     @InjectModel(LoginAttempt.name)
     private readonly loginAttemptModel: Model<LoginAttempt>,
   ) {}
 
+  // Create a new failed login attempt
   async create(failedLoginAttempt: LoginAttempt): Promise<LoginAttempt> {
+    this.logger.debug('Creating a new failed login attempt');
     const createdFailedLoginAttempt = new this.loginAttemptModel(
       failedLoginAttempt,
     );
     return createdFailedLoginAttempt.save();
   }
 
+  // Count recent failed login attempts by email within the specified time frame
   async countRecentAttemptsByEmail(
     email: string,
     timeFrameMinutes: number,
   ): Promise<number> {
+    this.logger.debug(
+      `Counting recent login attempts by email: ${email} within the past ${timeFrameMinutes} minutes`,
+    );
     const timeFrameDate = new Date(Date.now() - timeFrameMinutes * 60 * 1000);
     return this.loginAttemptModel.countDocuments({
       email,
@@ -29,10 +38,14 @@ export class LoginAttemptRepository implements ILoginAttemptRepository {
     });
   }
 
+  // Count recent failed login attempts by IP address within the specified time frame
   async countRecentAttemptsByIpAddress(
     ipAddress: string,
     timeFrameMinutes: number,
   ): Promise<number> {
+    this.logger.debug(
+      `Counting recent login attempts by IP address: ${ipAddress} within the past ${timeFrameMinutes} minutes`,
+    );
     const timeFrameDate = new Date(Date.now() - timeFrameMinutes * 60 * 1000);
     return this.loginAttemptModel.countDocuments({
       ipAddress,
--- src/infrastructure/mongo/login-attempt/login-attempt.schema.ts
+++ src/infrastructure/mongo/login-attempt/login-attempt.schema.ts
@@ -1,7 +1,12 @@
 import { Schema, SchemaFactory } from '@nestjs/mongoose';
 import { LoginAttempt } from '../../../domain/login-attempt/login-attempt.entity';
-
+import * as mongoose from 'mongoose';
 @Schema({ timestamps: { createdAt: 'createdAt' } })
 export class LoginAttemptDocument extends LoginAttempt {}
 
-export const LoginAttemptSchema = SchemaFactory.createForClass(LoginAttempt);
+export const LoginAttemptSchema = new mongoose.Schema({
+  id: String,
+  email: String,
+  ipAddress: String,
+  createdAt: Date,
+});
